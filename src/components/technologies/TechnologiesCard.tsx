527a8f14229b7aedd3176e0dc9afc1af3dfd55cd
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2026-09-13 23:38:53 +0600

    :art: styles: Updated icon

--- src/components/technologies/TechnologiesCard.tsx
+++ src/components/technologies/TechnologiesCard.tsx
@@ -15,8 +15,6 @@ const TechnologiesCard = ({
 
   return (
     <div className="w-full bg-white border border-gray-200 rounded-2xl p-5 shadow-sm hover:shadow-md transition">
-      
-      
       <div className="flex items-start justify-between mb-4">
         <div className="w-10 h-10 flex items-center justify-center rounded-lg bg-gray-50">
           <img
@@ -33,7 +31,6 @@ const TechnologiesCard = ({
         )}
       </div>
 
-  
       <h3 className="text-lg font-semibold text-gray-900 mb-1">
         {technology.name}
       </h3>
@@ -42,7 +39,7 @@ const TechnologiesCard = ({
         {technology.description}
       </p>
 
-      <div className="flex items-center gap-2 text-xs mb-5">
+      <div className="flex items-center gap-2 text-[11px] mb-5">
         <span className="px-2 py-1 rounded-md bg-gray-100 text-gray-600">
           {technology.category}
         </span>
@@ -52,15 +49,26 @@ const TechnologiesCard = ({
         </span>
 
         <div className="ml-auto flex items-center gap-1 text-gray-700 font-medium">
-          <span>★</span>
+          <span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              className="lucide fill-yellow-400 stroke-amber-400 lucide-star size-3"
+            >
+              <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z" />
+            </svg>
+          </span>
           <span>{technology.rating}</span>
         </div>
       </div>
 
-
       <button
         onClick={() => setSelectedStack(technology)}
-        
         className={`w-full text-xs sm:text-sm font-medium py-2.5 rounded-lg transition ${
           isAdded
             ? "bg-gray-100 text-gray-500 cursor-not-allowed"

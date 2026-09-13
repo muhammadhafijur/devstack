2bc4eb791ce180699d13dcacffd4053159a09c55
MuhammadHafijur <85216180+MuhammadHafijur@users.noreply.github.com>
2026-09-13 23:09:33 +0600

    :art: styles: Fix styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -92,7 +92,7 @@ export default function Footer() {
             </div>
 
             <div>
-              <p className="font-medium text-sm text-gray-900 dark:text-white">
+              <p className="font-medium text-sm text-gray-900 ">
                 COMPANY
               </p>
 
--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,24 +1,36 @@
-import { use } from 'react';
-import type { Itechnology } from '../../types/technologyType';
-import TechnologiesStack from './TechnologiesStack';
+import { use } from "react";
+import type { Itechnology } from "../../types/technologyType";
+import TechnologiesStack from "./TechnologiesStack";
 
 interface TechnologiesProps {
-    technologiesPromise: Promise<Itechnology[]>
+  technologiesPromise: Promise<Itechnology[]>;
 }
 
-const Technologies = ({technologiesPromise}: TechnologiesProps) => {
-    const technologies = use(technologiesPromise)
-    console.log(technologies)
+const Technologies = ({ technologiesPromise }: TechnologiesProps) => {
+  const technologies = use(technologiesPromise);
+  console.log(technologies);
 
-    return (
-        <div id="technologies" className="font-jakarta w-full max-w-7xl mx-auto px-4 sm:px-6 md:px-8 bg-white">
-            <div className="text-center md:text-start">
-                <h2 className="text-xl md:text-4xl font-bold">Explore the <span className='bg-linear-to-r from-[#EC4899] to-[#8B5CF6] bg-clip-text text-transparent'>Technologies</span></h2>
-                <p className="mt-2 text-[#64748B] text-sm md:text-base ">Pick one technology per category to build your ideal stack.</p>
-            </div>
-            <TechnologiesStack technologies={technologies}></TechnologiesStack>
+  return (
+    <section className="bg-white">
+      <div
+        id="technologies"
+        className="font-jakarta w-full max-w-7xl mx-auto px-4 sm:px-6 md:px-8 bg-white"
+      >
+        <div className="text-center md:text-start">
+          <h2 className="text-xl md:text-4xl font-bold">
+            Explore the{" "}
+            <span className="bg-linear-to-r from-[#EC4899] to-[#8B5CF6] bg-clip-text text-transparent">
+              Technologies
+            </span>
+          </h2>
+          <p className="mt-2 text-[#64748B] text-sm md:text-base ">
+            Pick one technology per category to build your ideal stack.
+          </p>
         </div>
-    );
+        <TechnologiesStack technologies={technologies}></TechnologiesStack>
+      </div>
+    </section>
+  );
 };
 
 export default Technologies;
